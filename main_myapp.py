eb240c95afa3a382055036334a4f7134a26c6116
aliyakalykulova <110055731+aliyakalykulova@users.noreply.github.com>
2025-12-22 22:52:10 +0500

    Update main_myapp.py

--- main_myapp.py
+++ main_myapp.py
@@ -142,8 +142,6 @@ df_standart = df_standart.merge(
     how='inner'
 )
 df_standart = df_standart.rename(columns={'Ingredient_pet_food': 'Ingredient'})
-df_standart = df_standart.set_index("Ingredient")
-
 
 proteins=df_standart[df_standart["Категория"].isin(["Мясо","Яйца и Молочные продукты"])]["Ingredient"].tolist()
 oils=df_standart[df_standart["Категория"].isin([ "Масло и жир"])]["Ingredient"].tolist()
