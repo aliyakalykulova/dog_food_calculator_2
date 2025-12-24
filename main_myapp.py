8e5add04b8eda720bad2c7f55cd121088dfb0a42
aliyakalykulova <110055731+aliyakalykulova@users.noreply.github.com>
2025-12-24 15:40:26 +0500

    Update main_myapp.py

--- main_myapp.py
+++ main_myapp.py
@@ -407,10 +407,9 @@ if user_breed:
 
             fat=sorted([i[0] for i in top_ings if i[0] in oils], key=lambda x: x[1], reverse=True)[:1]
             fat=df_standart[df_standart["Ingredient"].isin(fat)]["Standart"].tolist()
-			
 			wat=df_standart[df_standart["Ingredient"].isin(water)]["Standart"].tolist()
 			st.write(fat)
-            ingredients_finish = [i for i in prot+carb_cer+carb_veg+fat+wat if len(i)>0]
+			ingredients_finish = [i for i in prot+carb_cer+carb_veg+fat+wat if len(i)>0]
 			
             st.write(ingredients_finish)
 			
