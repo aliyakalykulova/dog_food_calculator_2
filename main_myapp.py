04c659b53cdf7518bbb8f184ae65ffc3bb1b07f7
aliyakalykulova <110055731+aliyakalykulova@users.noreply.github.com>
2025-12-24 16:43:25 +0500

    Update main_myapp.py

--- main_myapp.py
+++ main_myapp.py
@@ -407,7 +407,9 @@ if user_breed:
 
             fat=sorted([i[0] for i in top_ings if i[0] in oils], key=lambda x: x[1], reverse=True)[:1]
             fat=df_standart[df_standart["Ingredient"].isin(fat)]["Standart"].tolist()
-            ingredients_finish = [i for i in prot+carb_cer+carb_veg+fat+df_standart[df_standart["Ingredient"].isin(water)]["Standart"].tolist() if len(i)>0]
+             
+			wat=df_standart[df_standart["Ingredient"].isin(water)]["Standart"].tolist()
+            ingredients_finish = [i for i in prot+carb_cer+carb_veg+fat+wat if len(i)>0]
 			
             # 10.5) Display
             st.subheader("🌿 Рекомендуемые ингредиенты")
