24e726221081c3955acc878cfef1c54760d89b06
aliyakalykulova <110055731+aliyakalykulova@users.noreply.github.com>
2025-12-25 12:55:50 +0500

    Update main_myapp.py

--- main_myapp.py
+++ main_myapp.py
@@ -202,11 +202,8 @@ ingredient_models, frequent_ingredients = train_ingredient_models(food_df, X_com
 # Кнопки и состояния -----------------------------------------------------------------------------------
 # 1 этап выбор характеристик для собаки --------------------------------------------------------------
 
-if st.button("En"):
-	st.write("eee")
-if st.button("ru"):
-	st.write("rrrrr")
-
+if st.button("Нажми меня"):
+    st.write("Кнопка нажата")
 
 st.set_page_config(page_title="Рекомендации по питанию собак", layout="centered")
 st.header("Рекомендации по питанию собак")
