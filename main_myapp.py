1f15c981dc8649b56e92895375311cfcfabe516f
aliyakalykulova <110055731+aliyakalykulova@users.noreply.github.com>
2026-02-11 18:01:27 +0500

    Update main_myapp.py

--- main_myapp.py
+++ main_myapp.py
@@ -655,7 +655,7 @@ if user_breed:
                           needeble_proterin = protein_need_calc(st.session_state.kkal_sel, age_type_categ,  st.session_state.weight_sel, st.session_state.select_reproductive_status, age ,age_metric)					  
                           nutr_ranges['Влага'] = st.slider(f"{'Влага'}", 0, 100, (int(nutrient_preds["moisture"]-5), int(nutrient_preds["moisture"]+5)))
                           nutr_ranges['Белки'] = st.slider(f"{'Белки'}", 0, 100, (int(nutrient_preds["protein"]-3), int(nutrient_preds["protein"]+3)))
-                          nutr_ranges['Углеводы'] = st.slider(f"{'Углеводы'}", 0, 100, (int(nutrient_preds["carbohydrate (nfe)"]-2), int(nutrient_preds["carbohydrate (nfe)"]+2)))
+                          nutr_ranges['Углеводы'] = st.slider(f"{'Углеводы'}", 0, 100, (int(nutrient_preds["carbohydrate"]-2), int(nutrient_preds["carbohydrate"]+2)))
                           nutr_ranges['Жиры'] = st.slider(f"{'Жиры'}", 0, 100, (int(nutrient_preds["fat"]-1), int(nutrient_preds["fat"]+1)) )
 						  
                           if ingr_ranges != st.session_state.prev_ingr_ranges:
