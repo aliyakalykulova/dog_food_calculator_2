e12aa55794e2f307eb1efa194b5d047c48ea0392
aliyakalykulova <110055731+aliyakalykulova@users.noreply.github.com>
2026-02-12 13:36:06 +0500

    Update kcal_calculate.py

--- kcal_calculate.py
+++ kcal_calculate.py
@@ -70,7 +70,7 @@ def show_nutr_content(count_nutr_cont_all, other_nutrient_norms, nutrients_trans
                                                 norma = other_nutrient_norms[nutr_text[0]]
                                                 st.pyplot(bar_print(norma, count_nutr_cont_all.get(nutris, ''), nutr_text[0]+", "+ emg, str(emg)))
                                
-                                  st.markdown("#### 🪨 Минералы")
+                                  st.markdown("#### 🔹 Минералы")
                                   coli, colii=st.columns([6,3])
                                   with coli:
                                      for i in range(0, len(major_minerals)):
